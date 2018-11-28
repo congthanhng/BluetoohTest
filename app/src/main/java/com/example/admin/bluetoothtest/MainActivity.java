41205e594b0a0c127a7d4f3a53ad126b59f71f3a
nhocchoda <nhocchoda@gmail.com>
2018-11-28 11:29:50 +0700

    no message

--- app/src/main/java/com/example/admin/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/example/admin/bluetoothtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.admin.bluetoothtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox bluetoothCheckbox;
+    private RecyclerView bluetoothListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bluetoothCheckbox=findViewById(R.id.bluetooth_checkBox);
+        bluetoothListView=findViewById(R.id.bluetooth_listView);
     }
 }
